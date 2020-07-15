7d172e1b65b03b8bb060a3d809f1226374f87c40
vyklimavicius <vyklimavicius@gmail.com>
2020-07-15 11:53:06 -0500

    Basic Card Layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { DescriptionComponent } from './components/description/description.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -10,7 +13,10 @@ import { DescriptionComponent } from './components/description/description.compo
     DescriptionComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
