931f2c377d975f83ec261fb204f98a7ef2a91106
vyklimavicius <vyklimavicius@gmail.com>
2020-07-15 09:40:31 -0500

    Initial setup/Components folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { DescriptionComponent } from './components/description/description.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DescriptionComponent
   ],
   imports: [
     BrowserModule
